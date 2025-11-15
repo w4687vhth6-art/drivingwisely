1349032d93e5a8b801a82a74c926fdd845c7188d
Richard Griffiths <richard@vuwbo.com>
2025-11-15 14:54:27 +0000

    Added search and map windows

--- src/components/views/locations.js
+++ src/components/views/locations.js
@@ -5,7 +5,25 @@ import "./navbar.js";
 class LocationsComponent extends LitElement {
   static properties = {};
 
-  static styles = [sharedStyles];
+  static styles = [
+    sharedStyles,
+    css`
+      .dw-locations-container {
+        display: flex;
+        gap: 10px;
+        height: 40vh;
+        width: 100%;
+      }
+      .dw-locations-left-window {
+        flex: 1;
+        border: 1px solid black;
+      }
+      .dw-locations-right-window {
+        flex: 1;
+        border: 1px solid black;
+      }
+    `,
+  ];
 
   constructor() {
     super();
@@ -14,12 +32,24 @@ class LocationsComponent extends LitElement {
   render() {
     return html`
       <div class="dw-wrapper">
-      <dw-nav-bar></dw-nav-bar>
+        <dw-nav-bar></dw-nav-bar>
         <h1>Locations Page</h1>
 
-
-     
+        <div class="dw-locations-container">
+          <div class="dw-locations-left-window">
+            <div class="dw-locations-search">${this._searchInput()}</div>
+            <div class="dw-locations-results">RESULTS</div>
+          </div>
+          <div class="dw-locations-right-window">MAP</div>
+        </div>
       </div>
+    `;
+  }
+
+  _searchInput() {
+    return html`
+      <label for="name">Search:</label>
+      <input type="text" class="dw-input" id="name" name="name" />
     `;
   }
 }
