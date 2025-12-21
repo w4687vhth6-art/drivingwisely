bc774845b3eeb2d7bec23019ff96865b4ba5ff36
Richard Griffiths <rg_in_newcastle@yahoo.co.uk>
2025-12-21 20:31:44 +0000

    Flex

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -5,9 +5,32 @@ import "./navbar.js";
 class HomeComponent extends LitElement {
   static properties = {};
 
-  static styles = [sharedStyles];
+  static styles = [
+    sharedStyles,
+    css`
+      .box-container {
+        display: flex;
+        gap: 20px;
+        flex-direction: row;
+      }
 
-  
+      .box {
+        height: 100px;
+        width: 100px;
+        background-color: lightblue;
+        font-size: 24px;
+      }
+
+      media (width: 600px) {
+        .box-container {
+          flex-direction: column;
+        }
+        .box {
+          background-color: lightcoral;
+        }
+      }
+    `,
+  ];
 
   constructor() {
     super();
@@ -16,28 +39,22 @@ class HomeComponent extends LitElement {
   render() {
     return html`
       <div class="dw-wrapper">
-      <dw-nav-bar></dw-nav-bar>
+        <dw-nav-bar></dw-nav-bar>
         <h1>Driving Wisely</h1>
-     
 
-      <div class="container">
-      <div class="box" id="box1">1</div>
-      <div class="box" id="box2">2</div>
-      <div class="box" id="box3">3</div>
-          <label for="email">Email:</label>
-          <input
-            type="email"
-            class="dw-input"
-            id="email"
-            name="email"
-          
-          />
-<div class="password-wrapper">
-  <label>Password:</label>
-  <input type="password" />
-  
+        <div class="box-container">
+          <div class="box" id="box1">1</div>
+          <div class="box" id="box2">2</div>
+          <div class="box" id="box3">3</div>
+        </div>
 
-
+        <label for="email">Email:</label>
+        <input type="email" class="dw-input" id="email" name="email" />
+        <div class="password-wrapper">
+          <label>Password:</label>
+          <input type="password" />
+        </div>
+      </div>
     `;
   }
 }
--- src/styles/dw_component_styles.js
+++ src/styles/dw_component_styles.js
@@ -8,45 +8,31 @@ export const sharedStyles = [
     .dw-wrapper {
       margin: 1rem;
     }
-      h1 {
-        color: red;
-      }
-        button {
-          background-color: red;
-          border: none;
-          padding: 10px 20px;
-          font-size: 16px;
-          cursor: pointer;
-          height : 40px;
-          width : 200px;
-          border-radius : 8px;
-        }
-          .box{ 
-  height: 100px;
-  width: 100px;
-  background-color: lightblue;
-  margin: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-}
-
-{}
-</div>
-.center-form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh; 
-  gap: 20px;     
-.center-form input 
-  width: 300px;
-  padding: 12px 15px;
-  font-size: 18px;
-}
-  }
-
+    h1 {
+      color: red;
+    }
+    button {
+      background-color: red;
+      border: none;
+      padding: 10px 20px;
+      font-size: 16px;
+      cursor: pointer;
+      height: 40px;
+      width: 200px;
+      border-radius: 8px;
+    }
+    .center-form {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      gap: 20px;
+    }
+    .center-form input {
+      width: 300px;
+      padding: 12px 15px;
+      font-size: 18px;
+    }
   `,
 ];
