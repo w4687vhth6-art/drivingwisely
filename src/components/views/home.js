dafe26d75ab82a2771206eec6c203359fd782565
Ollie West <olliewest171@gmail.com>
2025-12-22 17:43:56 +0000

    call update

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -20,8 +20,13 @@ class HomeComponent extends LitElement {
         background-color: lightblue;
         font-size: 24px;
       }
-
-      media (width: 600px) {
+      .dw-login-label {
+      display: inline-block;
+      width: 100px;
+      font-size: 18px;
+      margin-bottom: 8px;
+    }
+      @media (max-width: 600px) {
         .box-container {
           flex-direction: column;
         }
@@ -48,13 +53,17 @@ class HomeComponent extends LitElement {
           <div class="box" id="box3">3</div>
         </div>
 
-        <label for="email">Email:</label>
-        <input type="email" class="dw-input" id="email" name="email" />
-        <div class="password-wrapper">
-          <label>Password:</label>
-          <input type="password" />
+        <div class="login-container">
+          <div class="email-wrapper">
+            <label for="email" class = "dw-login-label">Email:</label>
+            <input type="email" class="dw-input" id="email" name="email" />
+          </div>
+
+          <div class="password-wrapper">
+            <label for="password" class = "dw-login-label">Password:</label>
+            <input type="password" class="dw-input" id="password" name="password"  />
+          </div>
         </div>
-      </div>
     `;
   }
 }
